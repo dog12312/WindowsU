5f5564a60bfcf552640d66fb0c33a4edebcbedb8
dog12312 <dog12312@users.noreply.github.com>
2018-02-23 17:08:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 var Name = localStorage.getItem("Name");
 document.getElementById('AccountName').innerHTML = Name;
+
+function showBootScreen() {
+    document.getElementById("BootScreen").style.display = "block";
+}
+function closeBootScreen() {
+    document.getElementById("BootScreen").style.display = "none";
+}

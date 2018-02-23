ea02bf6d7389a2670eebd6de1e212ac140280c4d
dog12312 <dog12312@users.noreply.github.com>
2018-02-23 18:33:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function closeBootScreen() {
 }
 function showLoginScreen() {
     document.getElementById("Login").style.display = "block";
-    document.getElementById('AccountName').innerHTML = Name();
 }
 function closeLoginScreen() {
     document.getElementById("Login").style.display = "none";
@@ -21,3 +20,4 @@ function showDesktop() {
 function closeDesktop() {
     document.getElementById("Desktop").style.display = "none";
 }
+document.getElementById('AccountName').innerHTML = Name;

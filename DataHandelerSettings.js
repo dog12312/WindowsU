485e297c9d8e35017df7b858c9559189f3092945
dog12312 <dog12312@users.noreply.github.com>
2018-02-24 10:53:17 -0500

    Create DataHandelerSettings.js

---
+++ DataHandelerSettings.js
@@ -0,0 +1,12 @@
+var WindowUAccountPassword = localStorage.getItem("WindowUAccountPassword");
+
+function loopwinu() {
+  if (Screen == "WindowsUSettings") {
+    fill(500);
+    textSize(34);
+    text('Password',440,100);
+    textSize(20);
+    text('- press P to change',600,100);
+    text(WindowUAccountPassword,450,125);
+  }
+}

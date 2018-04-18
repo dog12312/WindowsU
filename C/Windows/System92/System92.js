64a91593f44a96e41921a096b7af72f64d35c8b7
dog12312 <dog12312@users.noreply.github.com>
2018-04-18 19:24:36 -0400

    Update System92.js

--- C/Windows/System92/System92.js
+++ C/Windows/System92/System92.js
@@ -38,7 +38,7 @@ if (WindowUAccountPassword != "" || WindowUAccountPassword != null) {
   if (PasswordEntered == WindowUAccountPassword) {
     hideLoginScreen();
     showDesktop();
-    loadapplication();
+    loaddesktopapplication();
   } else {alert('Wrong Password. ' + PasswordEntered + ' is not right.');}
 } else { hideLoginScreen();showDesktop(); }
 }

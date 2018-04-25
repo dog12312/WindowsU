19503529914c0a5a80cffe02eb6f7d42227a4326
dog12312 <dog12312@users.noreply.github.com>
2018-04-25 14:13:36 -0400

    Update LoadApplications.js

--- C/Windows/System92/LoadApplications.js
+++ C/Windows/System92/LoadApplications.js
@@ -7,11 +7,11 @@ function loaddesktopapplication() {
   document.getElementById("SearchIcon").style.display = "block";
   if (localStorage.WindowsU_ArcadeApplication == "Installed") {
     document.getElementById("Arcade").style.display = "block";
-  }
+  } else { document.getElementById("Arcade").style.display = "none"; }
   if (localStorage.WindowsU_p5js_editorApplication == "Installed") {
     document.getElementById("p5js_editor").style.display = "block";
-  }
+  } else { document.getElementById("p5js_editor").style.display = "none"; }
   if (localStorage.WindowsU_PokemonRedApplication == "Installed") {
     document.getElementById("PokemonRed").style.display = "block";
-  }
+  } else { document.getElementById("PokemonRed").style.display = "none"; }
 }

4b8a9e649cb6d26b3b4ca99785fe6ff9877457d5
dog12312 <dog12312@users.noreply.github.com>
2018-08-19 17:30:29 -0400

    Update System92.js

--- C/Windows/System92/System92.js
+++ C/Windows/System92/System92.js
@@ -76,9 +76,9 @@ if (WindowsUAccountPassword != "" || WindowsUAccountPassword != null) {
 
 var Window;
 function OpenWindow(url) {
-  clippy.load('Rover', function(agent) {
-      agent.play('Searching');
-  });
+  //clippy.load('Rover', function(agent) {
+      clippy.play('Searching');
+  //});
   document.getElementById("Window").style.display = "block";
   Window = document.getElementById("WindowContent");
   Window.src = url;
